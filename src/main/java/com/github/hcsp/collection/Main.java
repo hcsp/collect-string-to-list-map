ff9e3cd89433f905958fcead38929c1ae72915b3
DeeJayStudy <52043001+DeeJayStudy@users.noreply.github.com>
2019-09-02 17:31:53 +0800

    Update Main.java

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,13 @@
 package com.github.hcsp.collection;
 
+
+import java.util.Collections;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
+
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +17,19 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Collections.sort(users);
+        Map<String, List<User>> deptMap = new HashMap<>();
+        for(User user : users) {
+            if (!deptMap.containsKey(user.getDepartment())){
+                ArrayList<User> userArr = new ArrayList<>(){{add(user);}};
+                deptMap.put(user.getDepartment(), userArr);
+            }else {
+                deptMap.get(user.getDepartment()).add(user);
+            }
+        }
+        return deptMap;
+    }
 
     public static void main(String[] args) {
         System.out.println(
