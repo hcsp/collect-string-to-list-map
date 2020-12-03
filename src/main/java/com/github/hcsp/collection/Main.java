9b07091bc1bf3fa8b32c1fbd370223ace9d695f2
qipengWei <webqipeng@163.com>
2020-12-04 00:18:15 +0800

    分组排序

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,18 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String,List<User>> employees = new HashMap<>( );
+
+        users.forEach( user -> employees.put( user.getDepartment(), new ArrayList<>( )));
+        users.forEach( user -> {
+            employees.get( user.getDepartment() ).add( user );
+            employees.get( user.getDepartment() ).sort( Comparator.comparingInt( User::getAge ) );
+        } );
+
+
+        return employees;
+    }
 
     public static void main(String[] args) {
         System.out.println(
@@ -20,6 +29,7 @@ public class Main {
                         Arrays.asList(
                                 new User(1, "张三", 40, "技术部"),
                                 new User(2, "李四", 30, "技术部"),
-                                new User(3, "王五", 40, "市场部"))));
+                                new User(3, "王五", 40, "市场部"),
+                                new User(4, "王五", 50, "市场部"))));
     }
 }
