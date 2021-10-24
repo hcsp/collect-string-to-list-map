086c955a49d6dcd37a03dd4ff194296678cfb08b
zdmhahaha <987854018@qq.com>
2021-10-24 18:19:23 +0800

    更改

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,37 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Set<String> departmentName = new HashSet();
+        // 收集部门
+        for (User user : users) {
+            departmentName.add(user.getDepartment());
+        }
+        //创建一个键值对
+        Map<String, List<User>> map = new HashMap<>();
+        // 遍历所有的部门，再遍历所有对象将是这个的对象加到这个部门
+        for (String Dname : departmentName) {
+            ArrayList partlist = new ArrayList();
+            for (User user : users) {
+                if (Dname.equals(user.getDepartment())) {
+                    partlist.add(user);
+                }
+            }
+            Collections.sort(partlist, new sortage());
+            map.put(Dname,partlist);
+        }
+        return map;
+    }
+
+    private static class sortage implements Comparator<User> {
+        @Override
+        public int compare(User o1, User o2) {
+            if (o1.getAge() != o2.getAge()){
+                return (o1.getAge() - o2.getAge());
+            }
+            return 0;
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(
