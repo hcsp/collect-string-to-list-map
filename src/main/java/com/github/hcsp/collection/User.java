c5bac3649ce61be7a8bc25f40679b8f7af1e57d6
3stonelei <1462070965@qq.com>
2022-06-05 01:15:46 +0800

    ssssssuccess

--- src/main/java/com/github/hcsp/collection/User.java
+++ src/main/java/com/github/hcsp/collection/User.java
@@ -55,7 +55,7 @@ public class User{
     @Override
     public String toString() {
         return "{"  +
-                ", name='" + name + '\'' +
+                "name='" + name + '\'' +
                 ", department='" + department + '\'' +", age=" + age +
                 '}';
     }
