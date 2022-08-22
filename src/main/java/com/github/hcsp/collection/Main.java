f89680a98191b09bc0ac0c3100a342421514ddac
bomb <625750414@qq.com>
2022-08-22 20:57:57 +0800

    实现

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,6 +1,11 @@
 package com.github.hcsp.collection;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Collections;
+import java.util.Arrays;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -11,19 +16,19 @@ public class Main {
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
     public static Map<String, List<User>> collect(List<User> users) {
-        Collection departmentSet = new HashSet<String>();
+        List departments = new ArrayList();
         Map<String, List<User>> result = new HashMap<>();
 
-        for(User user:users){
+        for (User user : users) {
             System.out.println(user);
             String userDepartment = user.getDepartment();
-            if (departmentSet.contains(userDepartment)){
+            if (departments.contains(userDepartment)) {
                 List<User> userList = result.get(userDepartment);
                 userList.add(user);
                 Collections.sort(userList);
-            }else{
-                departmentSet.add(userDepartment);
-                result.put(userDepartment,new ArrayList<User>(Arrays.asList(user)));
+            } else {
+                departments.add(userDepartment);
+                result.put(userDepartment, new ArrayList<User>(Arrays.asList(user)));
             }
         }
         return result;
