7880a5f92f1c1f38461b36f6cca1bd24efc2622b
Jingxin Wang <wjxalexander@gmail.com>
2022-09-12 12:14:59 +0800

    done

--- src/main/java/com/github/hcsp/collection/User.java
+++ src/main/java/com/github/hcsp/collection/User.java
@@ -2,7 +2,7 @@ package com.github.hcsp.collection;
 
 import java.util.Objects;
 
-public class User {
+public class    User implements Comparable<User>{
     // 用户的id
     private final Integer id;
     // 用户的姓名
@@ -12,7 +12,7 @@ public class User {
     // 用户的部门，例如"技术部"/"市场部"
     private final String department;
 
-    public User(Integer id, String name, int age, String department) {
+    public User(Integer id, String name, int age, String department)  {
         this.id = id;
         this.name = name;
         this.age = age;
@@ -50,5 +50,14 @@ public class User {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+
+    @Override
+    public int compareTo(User o) {
+        if(this.equals(o)){
+            return  0;
+        }
+        return Integer.compare(this.getAge(), o.getAge());
     }
 }
